10a4c627d67dd78b0fc5d245608c12bcb6b5cd81
oooprya <oooprya@gmail.com>
2023-09-26 21:49:42 +0300

    perenos css

--- base/settings.py
+++ base/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "static", "templates/static"
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
