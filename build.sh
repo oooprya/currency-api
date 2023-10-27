8786f459e7e72a4b65308e03271838f75637f9d5
oooprya <oooprya@gmail.com>
2023-10-27 10:16:17 +0300

    add python manage.py makemigrations

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 python manage.py makemigrations
-python manage.py migrate
+# python manage.py migrate
