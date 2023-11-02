089b2d31fff6a6a5b704858b1bda146e9c86df3c
oooprya <oooprya@gmail.com>
2023-11-02 15:51:44 +0200

    ispravlenie

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 # exit on error
 
 python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py makemigrations
-python manage.py migrate
+# python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
