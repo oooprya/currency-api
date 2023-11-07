8d39406588e94bf6b0a16fedfe32bd135e11c6c2
oooprya <oooprya@gmail.com>
2023-11-07 11:19:10 +0200

    all Category

--- currency/admin.py
+++ currency/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Exchanger, Currency, CartItem, Category
+from .models import Exchanger, Currency, CartItem
 from django.contrib.auth.models import Permission
 
 admin.site.register(Permission)
@@ -15,7 +15,6 @@ class ExchangerAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Currency)
-admin.site.register(Category)
 admin.site.register(Exchanger, ExchangerAdmin)
 
 # Register your models here.
