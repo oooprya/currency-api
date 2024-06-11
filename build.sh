df36cf2e8c36cf2f2172a286d2e57896967d7fae
Boris <32761598+oooprya@users.noreply.github.com>
2024-06-11 16:33:27 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 
-# python manage.py collectstatic --no-input
-# python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
 # python manage.py makemigrations
 # python manage.py migrate
